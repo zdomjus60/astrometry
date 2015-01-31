4d9e9432befcb34714e6350831873a54e24ec9b6
zdomjus60 <domenico.mustara@libero.it>
2015-01-31 14:09:45 +0100

    Written test.py in a more pythonic way

--- test.py
+++ test.py
@@ -2,23 +2,9 @@
 # calculate Sun longitude according to Paul Schlyter formulas
 from trigon import *
 from planets import Planet
-sun = Planet('Sun', 1990, 4, 19)
-print sun.position()
-moon = Planet('Moon', 1990, 4, 19)
-print moon.position()
-mercury = Planet('Mercury', 1990, 4, 19)
-print mercury.position()
-venus = Planet('Venus', 1990, 4, 19)
-print venus.position()
-mars = Planet('Mars', 1990, 4, 19)
-print mars.position()
-jupiter = Planet('Jupiter', 1990, 4, 19)
-print jupiter.position()
-saturn = Planet('Saturn', 1990, 4, 19)
-print saturn.position()
-uranus = Planet('Uranus', 1990, 4, 19)
-print uranus.position()
-neptune = Planet('Neptune', 1990, 4, 19)
-print neptune.position()
-pluto = Planet('Pluto', 1990, 4, 19)
-print pluto.position()
+year, month, day = (1990, 4, 19)
+for planets in ('Sun', 'Moon', 'Mercury', 'Venus', 'Mars',
+               'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto'):
+    
+    planet = Planet(planets, year, month, day).position()
+    print planets, planet
