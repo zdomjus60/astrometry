be2202f68185a424207a3bc699fbc357a82d1b67
zdomjus60 <domenico.mustara@libero.it>
2015-02-18 16:35:47 +0100

    code refactoring to get back single attributes of planets

--- vsop87c/planets_sun.py
+++ vsop87c/planets_sun.py
@@ -39,22 +39,28 @@ class Planet:
                 x = body[0]-earth[0]
                 y = body[1]-earth[1]
                 z = body[2]-earth[2]
-            r = math.sqrt(x*x + y*y + z*z)
-            longitude = atan2(y,x) % 360 
-            latitude = atan2(z, math.sqrt(x*x + y*y))
+            self.r = math.sqrt(x*x + y*y + z*z)
+            self.longitude = atan2(y,x) % 360 
+            self.latitude = atan2(z, math.sqrt(x*x + y*y))
             obl_ecl = obl_ecl_Laskar(self.year, self.month, self.day, self.hour, self.minute, self.second)
-            f0 = sin(obl_ecl)*sin(longitude)*cos(latitude) + cos(obl_ecl)*sin(latitude)
-            f1 = cos(longitude)*cos(latitude)
-            f2 = cos(obl_ecl)*sin(longitude)*cos(latitude) - sin(obl_ecl)*sin(latitude)
-            RA = atan2(f2,f1) % 360
+            f0 = sin(obl_ecl)*sin(self.longitude)*cos(self.latitude) + cos(obl_ecl)*sin(self.latitude)
+            f1 = cos(self.longitude)*cos(self.latitude)
+            f2 = cos(obl_ecl)*sin(self.longitude)*cos(self.latitude) - sin(obl_ecl)*sin(self.latitude)
+            self.RA = atan2(f2,f1) % 360
             r0 = math.sqrt(f1*f1 + f2*f2)
-            Decl = atan2(f0,r0)
-            return (r, longitude, latitude, RA, Decl)
-        
+            self.Decl = atan2(f0,r0)
+           
+        return (self.r, self.longitude, self.latitude, self.RA, self.Decl)   
         
 if __name__ == '__main__':
     for i in ('Earth', 'Mercury', 'Venus', 'Mars', 'Jupiter',
               'Saturn', 'Uranus', 'Neptune'):
-        year, month, day, hour, minute, second = 1960,6,8,19,20,0
-        planet = Planet(i, year, month, day, hour, minute, second).calc()
-        print i, ddd2dms(planet[0]), ddd2dms(planet[1]), ddd2dms(planet[2]), ddd2dms(planet[3]/15), ddd2dms(planet[4])
+        year, month, day = 1680, 12, 10
+        j=i.lower()
+        if j == 'earth':
+            j = 'sun'
+        globals()[j] = Planet(i, year, month, day)
+
+    sun.calc(); mercury.calc(); venus.calc(); mars.calc();
+    jupiter.calc();saturn.calc(); uranus.calc(); neptune.calc()
+        
